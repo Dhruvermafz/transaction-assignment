ba84fae360fb3f4348016264b436fb1763ff0465
dhruvermafz <vermadhruv09112002@gmail.com>
2024-06-18 10:36:43 +0530

    adding sysytem

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import { store } from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = createRoot(document.getElementById("root"));
